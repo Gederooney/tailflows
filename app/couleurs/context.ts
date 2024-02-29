73c7fadb966b8f7736b1cace1245de4250f9c52f
gederooney <gyebadokpo61@gmail.com>
2024-02-29 18:31:25 -0500

    remove color folder from app

--- app/couleurs/context.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-import { createContext } from 'react'
-import { ContextState } from './colors'
-
-const intialState = {} as ContextState
-
-const ColorContext = createContext<ContextState>(intialState)
-
-export default ColorContext
--- app/couleurs/search/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Metadata, ResolvingMetadata } from 'next'
-import siteMetadata from '@/data/siteMetadata'
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
